24a5822904b65ad6dcc4a4398bd455b3e1639249
Charles Gourdin <gourdin.charles@gmail.com>
2019-12-11 14:44:30 +0100

    Commit avant ajout branch socket

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import ChatBox from './components/ChatBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
