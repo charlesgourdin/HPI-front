dc24cbddc2a28224d9553f67ff9129bfd1eb5f9c
Gérald Barbosa <barbosa.gerald@gmail.com>
2019-12-12 20:52:03 +0100

    filtrage basique de l'URL

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import ChatBox from './components/ChatBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const test_url = () => {
+
+    let params = (new URL(document.location)).searchParams;
+    let id = params.get('id');
+    let token = params.get('token');
+
+    // tests à personnaliser en fonction de l'url fournie :
+    // faire une requete SQL pour vérifier le token
+    if (token !== null && id !== null) {
+      return <ChatBox />
+    } else {
+      return (
+        <div id="access_denied">
+          <p>Vous n'êtes pas autorisé à accèder à ce lien</p>
+          <p>Retour à l'accueil</p>
+        </div>
+      );
+    }
+  }
+
   return (
     <div>
-      <ChatBox />
+      {test_url()}
     </div>
   );
 }
