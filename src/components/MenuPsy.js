0e978c938fec335a11502b68386413773533c35b
Charles Gourdin <gourdin.charles@gmail.com>
2020-01-10 15:36:11 +0100

    OK avant retro

---
+++ src/components/MenuPsy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MenuPsy = () => {
+
+    return(
+        <div 
+        className='h-100 w-100'
+        style={{
+            backgroundColor:'#236fff'
+        }}>
+
+        </div>
+    )
+}
+
+export default MenuPsy
--- src/pages/Psychologue.js
+++ src/pages/Psychologue.js
@@ -5,20 +5,26 @@ import {
 } from 'mdbreact';
 import ChatBox from '../components/ChatBox';
 import TicketFill from '../components/TicketFill';
-import StatusBloc from '../components/StatusBloc';
+import MenuPsy from '../components/MenuPsy';
 
 const Psychologue = () => {
 
 
     return (
-        <div className='d-flex h-100 p-2' style={{
+        <div className='d-flex h-100' style={{
         }}>
-            <MDBRow className='w-100'>
-                <MDBCol className='h-100 d-flex flex-column justify-content-between' size='6' md='5' xl='4'>
-                    {/* <StatusBloc /> */}
-                    <TicketFill />
+            <MDBRow className='w-100 h-100'>
+                <MDBCol className='h-100' size='6' md='5' xl='4'>
+                    <MDBRow className='w-100 h-100'>
+                        <MDBCol className='m-0 p-0 h-100' size='4' md='4' lg='3' xl='2'>
+                            <MenuPsy />
+                        </MDBCol>
+                        <MDBCol className='m-0 p-0 h-100' size='8' md='8' lg='9' xl='10'>
+                            <TicketFill />
+                        </MDBCol>
+                    </MDBRow>
                 </MDBCol>
-                <MDBCol className='h-100' size='6' md='7' xl='8'>
+                <MDBCol className='h-100 p-3' size='6' md='7' xl='8'>
                     <ChatBox />
                 </MDBCol>
             </MDBRow>
