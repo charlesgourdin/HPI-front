89cc1002b247119c22ce1cc5bab94a215903384c
Gérald Barbosa <barbosa.gerald@gmail.com>
2020-01-25 11:44:05 +0100

    ajout IP du server

--- src/providers/GlobalContext.js
+++ src/providers/GlobalContext.js
@@ -7,7 +7,8 @@ class GlobalProvider extends Component {
         this.state = {
             // endpoint: "http://192.168.146.52:4000",
             // endpoint: "http://192.168.146.94:4000",
-            endpoint: "http://192.168.1.12:4000",
+            // endpoint: "http://192.168.1.12:4000",
+            endpoint: "http://157.230.17.228",
         }
     }
 
