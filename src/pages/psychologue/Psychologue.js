268025b72f31df16101a4dfd0cb3c93fafa3675e
Charles Gourdin <gourdin.charles@gmail.com>
2020-01-26 16:12:44 +0100

    suppression de MDB dans psychologue

--- src/pages/psychologue/Psychologue.js
+++ src/pages/psychologue/Psychologue.js
@@ -22,25 +22,19 @@ const Psychologue = () => {
     }, [getTicket, getPsy])
 
     return (
-        <div className='d-flex h-100 justify-content-center' style={{}}>
-            <MDBRow className='w-100 h-100'>
-                <MDBCol className='h-100' size='6' md='5' xl='4'>
-                    <MDBRow className='w-100 h-100 '>
-                        <MDBCol className='m-0 p-0 h-100' style={{ maxWidth: '100px' }} size='4' md='4' lg='3' xl='3'>
-                            <MenuPsy />
-                        </MDBCol>
-                        <MDBCol className='m-0 p-0 h-100' size='8' md='8' lg='9' xl='9'>
-                            {menuActiv === 'profil' && <ProfilMenu />}
-                            {menuActiv === 'psychologues' && <PsychoMenu />}
-                            {menuActiv === 'tickets' && <TicketMenu />}
-                        </MDBCol>
-                    </MDBRow>
-                </MDBCol>
-                <MDBCol className='h-100 p-3' size='6' md='7' xl='8'>
-                    {chatActiv && <ChatBoxPsy />}
-                    {formActiv && <FormEndCall />}
-                </MDBCol>
-            </MDBRow>
+        <div className='d-flex h-100 w-100 justify-content-center' style={{}}>
+            <div className='m-0 p-0 h-100' style={{ width: '100px' }}>
+                <MenuPsy />
+            </div>
+            <div className='m-0 p-0 h-100' style={{ width: '600px'}}>
+                {menuActiv === 'profil' && <ProfilMenu />}
+                {menuActiv === 'psychologues' && <PsychoMenu />}
+                {menuActiv === 'tickets' && <TicketMenu />}
+            </div>
+            <div className='h-100 w-100 p-3'>
+                {chatActiv && <ChatBoxPsy />}
+                {formActiv && <FormEndCall />}
+            </div>
         </div>
     )
 }
